6d98d93b559a9a44d61e3c0b0c6ba68fdfcc54df
khairil0112 <sahrulsjdn3@gmail.com>
2025-07-25 17:47:59 +0700

    layouting

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -157,9 +157,6 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-   <?php
-   include_once("../aside/aside.php")
-   ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
